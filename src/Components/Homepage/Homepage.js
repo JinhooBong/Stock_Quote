acc25282bb249f1175e0479682850b53b37ed6ea
Jinhoo Bong <jinhoobong@gmail.com>
2020-07-21 20:23:46 -0700

    Initial project set-up

---
+++ src/Components/Homepage/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import StockHeader from '../StockHeader/StockHeader';
+
+function Homepage() {
+    return (
+        <div>
+            <StockHeader/>
+        </div>
+    )
+}
+
+export default Homepage;
---
+++ src/Components/StockHeader/StockHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from '../StockHeader/StockHeader.module.css';
+
+function StockHeader() {
+    return(
+        <div className={classes.Header}>
+            Stocks
+        </div>
+    )
+}
+
+export default StockHeader;
